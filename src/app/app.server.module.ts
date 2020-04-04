96b13b2c2a381389b07ea67cf0c4e04d4310b230
Mugunthan C <imugunthanc@gmail.com>
2020-04-04 16:34:01 +0530

    Initial commit

---
+++ src/app/app.server.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { AppModule } from "./app.module";
+import { AppComponent } from "./app.component";
+import { ServerModule } from "@angular/platform-server";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { UniversalInterceptor } from "./universal-interceptor";
+import { ModuleMapLoaderModule } from "@nguniversal/module-map-ngfactory-loader";
+
+@NgModule({
+  imports: [AppModule, ServerModule, ModuleMapLoaderModule],
+  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UniversalInterceptor,
+      multi: true,
+    },
+  ],
+})
+export class AppServerModule {}
